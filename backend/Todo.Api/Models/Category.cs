371d7e75c9bb54eccab77ef4613430aa0b680cb7
都力源 <mr.d@MRDMacBook-Pro.local>
2025-08-21 18:04:08 -0700

    Data model change

---
+++ backend/Todo.Api/Models/Category.cs
@@ -0,0 +1,11 @@
+namespace Todo.Api.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string CategoryTitle { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.UtcNow;
+    public DateTime ModifiedDateTime { get; set; } = DateTime.UtcNow;
+    public int Void{ get; set; }
+}
--- backend/Todo.Api/Models/TodoItem.cs
+++ backend/Todo.Api/Models/TodoItem.cs
@@ -4,6 +4,12 @@ public class TodoItem
 {
     public int Id { get; set; }
     public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
     public bool IsDone { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public int CategoryId{ get; set; }
+    public DateTime TaskStartTime { get; set; } = DateTime.UtcNow;
+    public DateTime TaskFinishTime { get; set; } = DateTime.UtcNow.AddHours(1);
+    public DateTime CreatedDateTime { get; set; } = DateTime.UtcNow;
+    public DateTime ModifiedDateTime { get; set; } = DateTime.UtcNow;
+    public int Void{ get; set; }
 }
